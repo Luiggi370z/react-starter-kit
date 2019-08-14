4195f730e7f076cf5d41201645699106591428e5
Luis Arpasi <meclipse@gmail.com>
2019-08-14 14:22:39 -0500

    Cleanup

--- src/store/modules/Users/selectors.js
+++ src/store/modules/Users/selectors.js
@@ -1,1 +1,15 @@
-store / modules / users / node_modules / reselect
+import { createSelector } from 'reselect'
+import { NAME } from '.'
+
+export const getAll = state => state[NAME]
+
+export const getCounts = createSelector(
+  getAll,
+  getCompleted,
+  getActive,
+  (allTodos, completedTodos, activeTodos) => ({
+    all: allTodos.length,
+    completed: completedTodos.length,
+    active: activeTodos.length
+  })
+)
