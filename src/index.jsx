2d3bf360e0d15fb0c73804291232464d5eabb028
Luis Arpasi <meclipse@gmail.com>
2019-08-14 22:07:10 -0500

    Cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// NOTE: Import any plugins initialization here.
+
 ReactDOM.render(<App />, document.getElementById('root'))
